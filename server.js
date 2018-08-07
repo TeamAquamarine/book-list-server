68cdb0e5bfa65afa72418c2f0ad241e106f939e5
Sharon Miller <sharon@sharonmillercreative.com>
2018-08-07 10:36:20 -0700

    add database to heroku and error check with load

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 const connectionString = `postgres://sharon:@localhost:5432/books_app`;
 const client = new pg.Client(connectionString);
 client.connect();
+client.on('error', err => console.log(err));
 
 app.get('/test', (req, res ) => res.send('hello world!!!') )
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
