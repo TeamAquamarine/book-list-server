6f25ba97234a55b48a46c33db2dc288fe8497658
Sharon Miller <sharon@sharonmillercreative.com>
2018-08-08 10:48:33 -0700

    Connect to server and add routes

--- server.js
+++ server.js
@@ -6,10 +6,20 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//connections
 const connectionString = `postgres://sharon:@localhost:5432/books_app`;
 const client = new pg.Client(connectionString);
 client.connect();
 client.on('error', err => console.log(err));
+
+//middleware
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 app.get('/api/v1/books', (req, res) => {
   let SQL = `SELECT book_id, title, author, img_url FROM books;`
@@ -20,12 +30,27 @@ app.get('/api/v1/books', (req, res) => {
 });
 
 app.get('/api/v1/books/:id', (req, res) => {
-  let SQL = `SELECT * FROM books WHERE book_id = $1`
+  let SQL = `SELECT * FROM books WHERE book_id = $1;`
   let values = [req.params.id];
 
   client.query(SQL, values)
   .then(results => res.send(results.rows))
   .catch(console.error);
 })
+
+app.post('/api/v1/books', (req, res) => {
+  let SQL = `INSERT INTO books(title, author, isbn, img_url, description) values($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING;`;
+
+  let values = [
+    req.body.title,
+    req.body.author,
+    req.body.isbn,
+    req.body.img_url,
+    req.body.description
+  ]
+
+  client.query(SQL, values)
+  .catch(console.error);
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
